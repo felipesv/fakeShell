c7bc7362f7a656e29d0be711c55f0634388172b8
Felipe Satizabal <1165@holbertonschool.com>
2019-11-18 19:46:05 +0000

    Add

--- _execve.c
+++ _execve.c
@@ -1,9 +1,31 @@
 #include "simple_shell.h"
 
-void _execve(char *arguments[])
+void _execve(char *prompt)
 {
-	if(execve(arguments[0], arguments, NULL) == -1)
+	char *arguments, **argum;
+	int cnt = 0, sizeArgum;
+
+	sizeArgum = countSpace(prompt);
+        argum = malloc(sizeof(char*) * ++sizeArgum);
+
+	if (argum == NULL)
+                perror("./hsh");
+
+	arguments = strtok(prompt, " \t\n\r");
+
+	while(arguments != NULL)
+	{
+                argum[cnt++] = arguments;
+                arguments = strtok(NULL, " \t\n\r");
+	}
+	argum[cnt] = NULL;
+
+	if(execve(argum[0], argum, NULL) == -1)
 	{
 		perror("Error");
 	}
+
+	free(argum);
+
+	exit (0);
 }
